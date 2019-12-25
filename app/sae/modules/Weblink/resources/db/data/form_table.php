abde25ef544bfa49f7dca1f64e3bb0b9679c3a65
vvigode <vvigode@mail.ru>
2019-12-25 23:21:36 +1000

    Update form_table.php

--- app/sae/modules/Weblink/resources/db/data/form_table.php
+++ app/sae/modules/Weblink/resources/db/data/form_table.php
@@ -5,11 +5,15 @@ $requests = [
     "DELETE FROM application_option WHERE code = 'prestashop';",
     "DELETE FROM application_option WHERE code = 'woocommerce';",
     "DELETE FROM application_option WHERE code = 'magento';",
+    "DELETE FROM application_option WHERE code = 'set_meal';",
+    "DELETE FROM application_option WHERE code = 'm_commerce';",
     "DELETE FROM acl_resource WHERE code = 'feature_shopify';",
     "DELETE FROM acl_resource WHERE code = 'feature_volusion';",
     "DELETE FROM acl_resource WHERE code = 'feature_prestashop';",
     "DELETE FROM acl_resource WHERE code = 'feature_woocommerce';",
     "DELETE FROM acl_resource WHERE code = 'feature_magento';",
+    "DELETE FROM acl_resource WHERE code = 'feature_set_meal';",
+    "DELETE FROM acl_resource WHERE code = 'feature_m_commerce';",
 ];
 
 foreach ($requests as $request) {
